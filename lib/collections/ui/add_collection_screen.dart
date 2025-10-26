cc5cc17c0723ec5e61d75016291b113c41734835
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-10-27 00:00:22 +0100

    Add a video player for each files hence have been picked

--- lib/collections/ui/add_collection_screen.dart
+++ lib/collections/ui/add_collection_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:alexandria/video/video_player.dart';
 import 'package:flutter/material.dart';
 
 import '../../common/result.dart';
@@ -9,6 +12,20 @@ class AddCollectionScreen extends StatefulWidget {
 }
 
 class _AddCollectionScreenState extends State<AddCollectionScreen> {
+  List<File> _files = [];
+
+  void _handleFiles(List<ItemFile?> files) {
+    List<File> filesToPlay = [];
+    for (var file in files) {
+      if (file != null) {
+        filesToPlay.add(File(file.path));
+      }
+    }
+    setState(() {
+      _files = filesToPlay;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -21,9 +38,7 @@ class _AddCollectionScreenState extends State<AddCollectionScreen> {
                   var pickedFiles = await FileItemPicker().pickFiles();
                   switch (pickedFiles) {
                     case Ok<List<ItemFile?>>():
-                      for (var file in pickedFiles.value) {
-                        print(file);
-                      }
+                      _handleFiles(pickedFiles.value);
                     case Error<List<ItemFile?>>():
                       throw UnimplementedError();
                   }
@@ -31,6 +46,22 @@ class _AddCollectionScreenState extends State<AddCollectionScreen> {
                 label: Text('...browse'),
               ),
             ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              itemCount: _files.length,
+              itemBuilder: (context, index) {
+                final file = _files[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 6.0,
+                    horizontal: 8,
+                  ),
+                  child: Row(children: [VideoPlayerWidget(videoFile: file)]),
+                );
+              },
+            ),
           ),
         ],
       ),
---
+++ lib/video/video_player.dart
@@ -0,0 +1,76 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerWidget extends StatefulWidget {
+  final File videoFile;
+
+  const VideoPlayerWidget({super.key, required this.videoFile});
+
+  @override
+  State<VideoPlayerWidget> createState() => _VideoPlayerState();
+}
+
+class _VideoPlayerState extends State<VideoPlayerWidget> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.file(widget.videoFile)
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      height: 200,
+      child: Stack(
+        fit: StackFit.loose,
+        children: [
+          Positioned.fill(
+            child: _controller.value.isInitialized
+                ? VideoPlayer(_controller)
+                : Container(),
+          ),
+          Align(
+            alignment: Alignment(0, 0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black.withValues(
+                  alpha: 0.5,
+                  blue: 0.5,
+                  green: 0.5,
+                  red: 0.5,
+                ),
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(12),
+                elevation: 0,
+              ),
+              onPressed: () {
+                setState(() {
+                  _controller.value.isPlaying
+                      ? _controller.pause()
+                      : _controller.play();
+                });
+              },
+              child: Icon(
+                _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
