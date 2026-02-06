a4f65c8efcd5e5b8078f1643195e072edbe2c8f4
Bertrand Bougon <bertrand.bougon@gmail.com>
2026-02-06 22:03:35 +0100

    Allow video to be played on any OS
    ... the videos where allowed to be access on the OS file system from the cache directory which works fine when creating a video.
    When playing a video from the CollectionDetailsPage did not work anymore as the video was not in the cache directory anymore.
    Now it ensures each time a video is created, the path access is allowed

--- src-tauri/src/collections/tauri_commands.rs
+++ src-tauri/src/collections/tauri_commands.rs
@@ -3,13 +3,16 @@ use crate::collections::video::{ThumbnailItem, VideoCollectionToUpdate, VideoFil
 use crate::event_bus::{EventBusManager, TauriEventBus};
 use crate::infra::files::file_manager::FileManagerForHardDrive;
 use crate::repositories::repositories;
-use tauri::AppHandle;
+use tauri::{AppHandle, Manager, Runtime};
 
 #[tauri::command]
 pub async fn create_collection(
     app: AppHandle,
     paths: Vec<String>,
 ) -> Result<Vec<ThumbnailItem>, String> {
+    for path in &paths {
+        allow_path(&app, path)?;
+    }
     let video_file_manager = VideoFileManager::new(Box::new(FileManagerForHardDrive::new()));
     let collection = CollectionService::create_collection(
         paths,
@@ -29,6 +32,7 @@ pub async fn create_collection(
 
 #[tauri::command]
 pub async fn update_video(app: AppHandle, video: VideoCollectionToUpdate) -> Result<(), String> {
+    allow_path(&app, video.video.path.to_str().unwrap())?;
     CollectionService::update_video(
         video,
         EventBusManager::new(Box::new(TauriEventBus::new(app))),
@@ -37,7 +41,18 @@ pub async fn update_video(app: AppHandle, video: VideoCollectionToUpdate) -> Res
 }
 
 #[tauri::command]
-pub async fn get_collections(_app: AppHandle) -> Result<Vec<Collection>, String> {
+pub async fn get_collections(app: AppHandle) -> Result<Vec<Collection>, String> {
     let collections = repositories().collections().list();
+    for c in &collections {
+        for v in &c.videos {
+            allow_path(&app, v.path.to_str().unwrap())?;
+        }
+    }
     Ok(collections)
+}
+
+fn allow_path<R: Runtime>(app: &AppHandle<R>, path: &str) -> Result<(), String> {
+    app.asset_protocol_scope()
+        .allow_file(path)
+        .map_err(|e: tauri::Error| e.to_string())
 }
