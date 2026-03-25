266752803303431eea1a94168d66e5c0217a0dfc
Bertrand Bougon <bertrand.bougon@gmail.com>
2026-03-25 21:49:46 +0100

    Emit an event VideoDataRetrieved when video data raw information are retrieved
    ... for future collection creation

--- src-tauri/src/event_bus.rs
+++ src-tauri/src/event_bus.rs
@@ -1,4 +1,6 @@
+use serde::Serialize;
 use serde_json::Value;
+use std::sync::Arc;
 
 #[derive(Clone)]
 pub struct Event {
@@ -9,12 +11,20 @@ pub trait EventBus: Send + Sync {
     fn publish(&self, event: Event);
 }
 
+#[derive(Clone)]
 pub struct EventBusManager {
-    pub event_bus: Box<dyn EventBus>,
+    pub event_bus: Arc<dyn EventBus>,
 }
 
 impl EventBusManager {
-    pub fn new(event_bus: Box<dyn EventBus>) -> Self {
+    pub fn new(event_bus: Arc<dyn EventBus>) -> Self {
         Self { event_bus }
+    }
+
+    pub fn publish<T: Serialize>(&self, event_type: &str, data: T) {
+        self.event_bus.publish(Event {
+            event_type: event_type.to_string(),
+            data: serde_json::to_value(data).unwrap(),
+        });
     }
 }
